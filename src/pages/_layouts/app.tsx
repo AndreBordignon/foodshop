7c682bbe471f435b0fbf4125dcc3804532eed02c
André Bordignon <andre.bordignon@ciss.com.br>
2024-01-31 16:27:03 -0300

    add validation from statuscode

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,7 +1,37 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { Header } from "@/components/header";
+import { useLayoutEffect } from "react";
+import { AxiosResponse, isAxiosError } from "axios";
+import { api } from "@/lib/axios";
 
 export function AppLayout() {
+  const navigate = useNavigate();
+  useLayoutEffect(() => {
+    const interceptorId = api.interceptors.response.use(
+      (response: AxiosResponse) => response,
+      (error: { response: { status: any; data: { code: any } } }) => {
+        if (isAxiosError(error)) {
+          console.log(error);
+          const status = error.response?.status;
+          const code = error.response?.data.message;
+
+          if (status === 401 && code === "Unauthorized") {
+            navigate("/sign-in", {
+              replace: true,
+            });
+          }
+        }
+
+        return Promise.reject(error);
+      },
+    );
+
+    // Clean up the side effect when the component unmounts
+    return () => {
+      api.interceptors.response.eject(interceptorId);
+    };
+  }, [navigate]);
+
   return (
     <div className="flex flex min-h-screen flex-col antialiased">
       <Header />
